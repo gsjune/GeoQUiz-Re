802803c9096e25c9720d67a58d5a147b728cd490
Hongchul Kim <gsjune@naver.com>
2017-07-30 16:39:49 +0900

    CheatActivity에서 엑스트라 상수 추가하기와 새로운 newIntent(...) 메소드

--- src/main/java/com/hckim/geoquizre/CheatActivity.java
+++ src/main/java/com/hckim/geoquizre/CheatActivity.java
@@ -1,9 +1,19 @@
 package com.hckim.geoquizre;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class CheatActivity extends AppCompatActivity {
+
+    private static final String EXTRA_ANSWER_IS_TRUE = "com.hckim.geoquizre.answer_is_true";
+
+    public static Intent newIntent(Context packageContext, boolean answerIsTrue) {
+        Intent intent = new Intent(packageContext, CheatActivity.class);
+        intent.putExtra(EXTRA_ANSWER_IS_TRUE, answerIsTrue);
+        return intent;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
